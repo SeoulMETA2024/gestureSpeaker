2e632c14254ca7615a0fc87c3b110b74ed8cbc44
ehdrjs13 <ehdrjs070102@gmail.com>
2024-05-13 23:50:44 +0900

    .

---
+++ music_controller/music_controller.py
@@ -0,0 +1,30 @@
+class MusicControll:
+    def __init__(self) -> None:
+        import pygame
+
+
+        pass
+
+    def music_stop(self):
+        
+        #음악 정지
+
+        return
+    
+    def music_start(self):
+
+        #음악 시작
+
+        return
+
+    def next_song(self):
+
+        #다음 곡 재생
+
+        return
+    
+    def previous_song(self):
+
+        #이전 곡 재생
+
+        return
