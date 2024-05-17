533642da37777af374bdd6519211d58f5391c10f
ehdrjs13 <ehdrjs070102@gmail.com>
2024-05-17 14:22:21 +0000

    implement music stop/pause

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
 from music_controller.music_controller import MusicControll
-from hand_detection.handDetection import
+from flask import Flask
+
+
+MUSIC_LIST = [] #샘플 음악 파일 입력
+
+music = MusicControll(MUSIC_LIST)
+
+app = Flask(__name__)
+
+@app.route('/')
+def ConnectionTest():
+    return "Success"
+
+@app.route('/start/')
+def StartMusic():
+    music.music_start()
+
+@app.route('/pause/')
+def PauseMusic():
+    music.music_stop()
+
+@app.route('/next/')
+def StartMusic():
+    music.next_song()
+
+@app.route('/previous/')
+def StartMusic():
+    music.previous_song()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
--- music_controller/music_controller.py
+++ music_controller/music_controller.py
@@ -1,19 +1,41 @@
 class MusicControll:
-    def __init__(self) -> None:
+    def __init__(self, file_name) -> None:
         import pygame
+
+        file_list = [file_name[1], file_name[2], file_name[3]]
+
+        pygame.mixer.init()
+
+        self.music_init()
+
 
 
         pass
 
-    def music_stop(self):
+    def music_init(self, music_file):
+        #기본 음악 불러오기
+
+        pygame.mixer.music.load(music_file)
+
+        pygame.mixer.music.play()
+        pygame.mixer.music.pause()
         
+
+    def music_stop(self):
         #음악 정지
+        
+        pygame.mixer.music.pause()
+        
+        
 
         return
     
     def music_start(self):
-
         #음악 시작
+        
+        pygame.mixer.music.play()
+
+        
 
         return
 
